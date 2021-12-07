b14eb6e2187662f08422fab11560080682c99b65
Naufalhamidd <hamidnaufal217@gmail.com>
2021-12-07 08:34:14 +0700

    membuat view create.blade

---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,58 @@
+@extends('dashboard.layouts.main')
+@section('container')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Create New Post</h1>
+</div>
+
+@if(session()->has('success'))
+  <div class="alert alert-success" role="alert">
+    {{ session('success') }}
+  </div>
+@endif
+
+<div class="col-md-8">
+  <form method="post" action="/dashboard/posts" class="mb-5" enctype="multipart/form-data">
+    @csrf
+      <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{    old('title') }}">
+        @error('title')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+      <div class="mb-3">
+        <label for="slug" class="form-label">Slug</label>
+        <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug') }}">
+        @error('slug')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+      <div class="mb-3">
+        <label for="image" class="form-label">Post Image</label>
+        <img class="img-preview img-fluid mb-3 col-sm-5">
+        <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+        @error('image')
+        <div class="invalid-feedback">
+          {{ $message }}
+        </div>
+        @enderror
+      </div>
+      <div class="mb-3">
+          <label for="body" class="form-label">Body</label>
+          @error('body')
+            <p class="text-danger">{{ $message }}</p>
+          @enderror
+              <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+              <trix-editor input="body"></trix-editor>         
+        </div>
+      
+      
+      <button type="submit" class="btn btn-primary">Create Post</button>
+    </form>
+  
+</div>
+@endsection
