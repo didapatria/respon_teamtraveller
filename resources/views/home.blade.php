ed791053d386196244654135117e6071780a736c
MuhammadRestu19 <muhammadrestu1909@gmail.com>
2021-12-16 06:28:47 +0700

    memodifikasi konten home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
     </div>
 
     <div class="row mb-4">
-      <h1 class="mb-4">Latest Post Review Smartphone</h1>
+      <h1 class="mb-5 pb-2 border-bottom">Latest Post Review Smartphone</h1>
       @if ($posts->count())
       <div class="col-md-6 mb-3">
         <div class="card bg-orange">
@@ -72,31 +72,104 @@
 
   </div>
     
-    <div class="bg-orange">
-      <div class="container">
-        <div class="row">
-          <h1 class="my-4">Popular Prand</h1>
-          @foreach ($brands as $brand)
-            <div class="col-md-2 mb-3">
-              <a href="/?brand={{ $brand->slug }}">
-                <div class="card bg-dark text-white">
-                  @if ($brand->image)
-                    <img src="{{ asset('storage/' . $brand->image) }}" alt="{{ $brand->name }}" class="card-img brand-img ratio">
-                  @else
-                    <img src="https://source.unsplash.com/1600x900?{{ $brand->name }}" class="card-img brand-img ratio" alt="{{ $brand->name }}">
-                  @endif
-                  <div class="card-img-overlay d-flex align-items-end p-0">
-                    <h5 class="card-title text-center flex-fill p-2 fs-3" style="background-color: rgba(30, 30, 30, 0.7)">{{ $brand->name }}</h5>
-                  </div>
+  <div class="b-example-divider"></div>
+
+  <div class="bg-orange">
+    <div class="container">
+      <div class="row">
+        <h1 class="mb-5 pb-2 border-bottom">Popular Prand</h1>
+        @foreach ($brands as $brand)
+          <div class="col-md-2 mb-3">
+            <a href="/?brand={{ $brand->slug }}">
+              <div class="card bg-dark text-white">
+                @if ($brand->image)
+                  <img src="{{ asset('storage/' . $brand->image) }}" alt="{{ $brand->name }}" class="card-img brand-img ratio">
+                @else
+                  <img src="https://source.unsplash.com/1600x900?{{ $brand->name }}" class="card-img brand-img ratio" alt="{{ $brand->name }}">
+                @endif
+                <div class="card-img-overlay d-flex align-items-end p-0">
+                  <h5 class="card-title text-center flex-fill p-2 fs-3" style="background-color: rgba(30, 30, 30, 0.7)">{{ $brand->name }}</h5>
                 </div>
-              </a>
-            </div>
-          @endforeach
+              </div>
+            </a>
+          </div>
+        @endforeach
+      </div>
+      {{-- Pagination --}}
+      <div class="d-flex justify-content-end pb-4">
+        {{ $brands->links() }}
+      </div>
+    </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+  <div class="container px-4 py-5" id="custom-cards">
+    <h2 class="pb-2 border-bottom">Custom cards</h2>
+
+    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('/assets/img/unsplash/unsplash-photo-1.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h2>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+                <img src="/assets/img/respon-logo.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+              </li>
+              <li class="d-flex align-items-center me-3">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                <small>Earth</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
+                <small>3d</small>
+              </li>
+            </ul>
+          </div>
         </div>
-        {{-- Pagination --}}
-        <div class="d-flex justify-content-end pb-4">
-          {{ $brands->links() }}
+      </div>
+
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('/assets/img/unsplash/unsplash-photo-2.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Much longer title that wraps to multiple lines</h2>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+                <img src="/assets/img/respon-logo.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+              </li>
+              <li class="d-flex align-items-center me-3">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                <small>Pakistan</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
+                <small>4d</small>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('/assets/img/unsplash/unsplash-photo-3.jpg');">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h2>
+            <ul class="d-flex list-unstyled mt-auto">
+              <li class="me-auto">
+                <img src="/assets/img/respon-logo.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+              </li>
+              <li class="d-flex align-items-center me-3">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                <small>California</small>
+              </li>
+              <li class="d-flex align-items-center">
+                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
+                <small>5d</small>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
+  </div>
 @endsection
